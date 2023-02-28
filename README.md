319c85e32b85fff2a8324ebd8c78c3f5aa0fb7f6
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-02-28 18:38:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Astro Smashing Workshop
+
+## Setup
+
+Run `npm install && npm run dev` and replace the access token in `astro.config.mjs` with the one from your newly created space.
+
+## Setting up the Storyblok Block Schema
+
+You may use`storyblok-components.json` and the [push-components command of the Storyblok CLI](https://github.com/storyblok/storyblok-cli#push-components) in order to recreate the block schema.
---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import vue from '@astrojs/vue'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: 'Gba8ER5B7POJ9gjwteGduAtt',
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        tabbed_content: 'storyblok/TabbedContent',
+      },
+    }),
+    tailwind(),
+    vue(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
